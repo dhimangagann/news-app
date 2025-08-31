2fd1b9515c25410a35ac1b5b7991729d25b82bc7
[Gagandhiman] <[bilkhugagan@gmail.com]>
2025-08-31 22:47:59 +0530

    fix hooks error

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,4 +1,4 @@
-import { useTheme } from '../contexts/ThemeContext'
+import { useTheme } from '../hooks/useTheme'
 
 const ThemeToggle = () => {
   const { isDarkMode, toggleTheme } = useTheme()
--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -1,14 +1,8 @@
-import { createContext, useContext, useState, useEffect } from 'react'
+import { createContext, useState, useEffect } from 'react'
 
 const ThemeContext = createContext()
 
-export const useTheme = () => {
-  const context = useContext(ThemeContext)
-  if (!context) {
-    throw new Error('useTheme must be used within a ThemeProvider')
-  }
-  return context
-}
+export { ThemeContext }
 
 export const ThemeProvider = ({ children }) => {
   const [isDarkMode, setIsDarkMode] = useState(false)
